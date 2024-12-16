183a84208accda8ad9d90d1d553f742d2998e1eb
redburning <253058773@qq.com>
2024-12-16 10:48:15 +0800

    修复了一些已知问题

--- src/main/java/database/ddl/transformer/TestTransformer.java
+++ src/main/java/database/ddl/transformer/TestTransformer.java
@@ -1,45 +1,54 @@
 package database.ddl.transformer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import database.ddl.transformer.bean.DataBaseConnection;
+import database.ddl.transformer.bean.DataBaseType;
+import database.ddl.transformer.core.transform.DdlExecutor;
 import database.ddl.transformer.core.transform.Transformer;
 
 public class TestTransformer {
 
+	private static final Logger logger = LoggerFactory.getLogger(TestTransformer.class);
+	
 	public static DataBaseConnection mysqlConn = DataBaseConnection.builder()
-			.jdbcUrl("jdbc:mysql://127.0.0.1:3306")
-			.user("username")
-			.password("password")
+			.jdbcUrl("jdbc:mysql://10.4.45.207:3306")
+			.user("root")
+			.password("aaa123+-*/")
 			.build();
 	
 	public static DataBaseConnection mysqlConn2 = DataBaseConnection.builder()
-			.jdbcUrl("jdbc:mysql://127.0.0.1:3308")
-			.user("username")
-			.password("password")
+			.jdbcUrl("jdbc:mysql://10.4.130.17:3306")
+			.user("root")
+			.password("123456")
 			.build();
 	
 	public static DataBaseConnection oracleConn = DataBaseConnection.builder()
-			.jdbcUrl("jdbc:oracle:thin:@127.0.0.1:1521/cdc")
-			.user("username")
-			.password("password")
+			.jdbcUrl("jdbc:oracle:thin:@10.4.45.206:1522/cdc")
+			.user("dxp")
+			.password("123456")
 			.build();
 	
 	public static DataBaseConnection postgresqlConn = DataBaseConnection.builder()
-			.jdbcUrl("jdbc:postgresql://127.0.0.1:5432/postgres")
-			.user("username")
-			.password("password")
+			.jdbcUrl("jdbc:postgresql://10.4.79.24:5432/postgres")
+			.user("postgres")
+			.password("Psd1@3+-*/")
 			.build();
 	
 	public static DataBaseConnection damengConn = DataBaseConnection.builder()
-			.jdbcUrl("jdbc:dm://127.0.0.1:5236")
-			.user("username")
-			.password("password")
+			.jdbcUrl("jdbc:dm://10.4.45.207:5236")
+			.user("CDC")
+			.password("cDC%%2024")
 			.build();
 	
 	public static void testMySQL2Oracle() throws Exception {
-		String createTableDDL = new Transformer(mysqlConn, oracleConn)
-				.transformCreateTableDDL("cdc_test_source", ".*")    // .* for all tables
+		String createTableDDL = new Transformer(mysqlConn, DataBaseType.ORACLE)
+				.transformCreateTableDDL("cdc_test_source", "test_table_.*")    // .* for all tables
 				.getTransformedCreateTableDDL();
 		System.out.println(createTableDDL);
+		
+		new DdlExecutor(oracleConn).executeSql(createTableDDL);
 	}
 	
 	public static void testOracle2MySQL() throws Exception {
@@ -84,6 +93,13 @@ public class TestTransformer {
 		System.out.println(createTableDDL);
 	}
 	
+	public static void testMySQL2Dameng() throws Exception {
+		String createTableDDL = new Transformer(mysqlConn, damengConn)
+				.transformCreateTableDDL("cdc_test_source", "stress_testing.*")
+				.getTransformedCreateTableDDL();
+		logger.info(createTableDDL);
+	}
+	
 	public static void testDameng2Oracle() throws Exception {
 		String createTableDDL = new Transformer(damengConn, oracleConn)
 				.transformCreateTableDDL("CDC", "TEST_TABLE_1")
@@ -98,8 +114,9 @@ public class TestTransformer {
 		System.out.println(createTableDDL);
 	}
 	
+	
 	public static void main(String[] args) throws Exception {
-		testMySQL2Oracle();
+		testMySQL2Dameng();
 		// testOracle2MySQL();
 		// testMySQL2PostgreSQL();
 		// testPostgreSQL2MySQL();
